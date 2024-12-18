13d4d123df28a60259c79f674fe5b2d74428f4b5
James D <66693959+James-Da@users.noreply.github.com>
2024-12-18 17:12:45 +0100

    added fonts and colors to tailwind config

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,16 @@ import React from "react";
 const App = () => {
 	return (
 		<main>
-			<h1 className="text-5xl text-orange-500 font-bold">
+			<h1 className="text-5xl text-violet-300 font-bold font-robert-medium">
+				Welcome to Awwwards
+			</h1>
+			<h1 className="text-5xl text-orange-500 font-circular-web">
+				Welcome to Awwwards
+			</h1>
+			<h1 className="text-5xl text-orange-500 font-robert-medium">
+				Welcome to Awwwards
+			</h1>
+			<h1 className="text-5xl text-orange-500 font-robert-regular">
 				Welcome to Awwwards
 			</h1>
 		</main>
